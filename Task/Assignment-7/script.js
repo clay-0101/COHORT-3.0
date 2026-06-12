1fdbfb5d1b7d4f5db99e652fd494fe361baaa383
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-06-12 17:46:21 +0530

    Add Delete Button

--- Task/Assignment-7/script.js
+++ Task/Assignment-7/script.js
@@ -3,16 +3,35 @@ const btn = document.querySelector('#addBtn')
 const todoList = document.querySelector('#todo-list')
 
 
+
 btn.addEventListener('click', () => {
     let value = input.value
-    if(value.trim() == "") return
-    todoList.innerHTML += `<div class="li">
-                    <p>${value}</p>
-                    <div>
+    if (value.trim() == "") return
+
+
+    //Creating Element li
+    let task = document.createElement('div')
+    task.classList.add('li')
+
+    task.innerHTML = `
+                    <div class='pParent'>
+                    <p class='para'>${value}</p>
+                    </div>
+
+                    <div'>
                         <button class="btn btn1">Edit</button>
                         <button class="btn btn2">Delete</button>
-                    </div>
-                </div>`
-    input.value = ""
+                    </div>`
 
+    todoList.appendChild(task)
+
+    //For Delete The Task
+    todoList.addEventListener('click', (e) => {
+        if (e.target.classList.contains('btn2')) {
+            let child = e.target.closest('.li')
+            todoList.removeChild(child)
+        }
+    })
+
+    input.value = ""
 })
