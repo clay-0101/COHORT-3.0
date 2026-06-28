07309650868009a4017c7fd0b6ce5fd91b3c5794
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-06-28 21:50:02 +0530

    Add user using registration form

---
+++ Expense Tracker/script.js
@@ -0,0 +1,57 @@
+const login_Form = document.querySelector('#loginAuth')
+const register_Form = document.querySelector('#registerAuth')
+const goToLogin_Form = document.querySelector('#loginHere')
+const goToRegister_Form = document.querySelector('#goToRegistration')
+const register_username = document.querySelector('#Rusername')
+const register_password = document.querySelector('#Rpassword')
+
+
+let users = []
+
+
+function show_User() {
+    // users.forEach((user) => {
+    //     console.log(user)
+    // })
+    console.log(users)
+}
+show_User()
+
+
+// REGISTRATION FORM
+register_Form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let user_exists = users.some(user => user.username === register_username.value)
+
+    if (user_exists) {
+        alert('This Username is already exists..')
+    } else {
+        users.push({
+            id: Date.now(),
+            username: register_username.value,
+            password: register_password.value,
+            current_balance: 0,
+            total_income: 0,
+            total_expense: 0,
+            total_transactions: 0
+        })
+        show_User()
+    }
+
+
+    register_Form.reset()
+})
+
+
+
+
+// SHOW / HIDDEN - LOGIN & REGISTER FORM
+goToLogin_Form.addEventListener('click', () => {
+    login_Form.style.display = 'block'
+    register_Form.style.display = 'none'
+})
+goToRegister_Form.addEventListener('click', () => {
+    register_Form.style.display = 'block';
+    login_Form.style.display = 'none';
+});
