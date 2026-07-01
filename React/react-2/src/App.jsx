49f3bbde5f1b0b15eb67d7b4f5fc0f268a01d3f9
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-07-01 20:58:58 +0530

    How components works

---
+++ React/react-2/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './Components/hero'
+import Navbar from './Components/navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Hero/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ React/react-2/src/Components/hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const hero = () => {
+  return (
+    <div>
+        <h1>This is my Hero Component</h1>
+    </div>
+  )
+}
+
+export default hero
