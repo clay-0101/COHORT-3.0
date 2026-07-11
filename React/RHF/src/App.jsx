f7559a142fe3b71aaa1e95870c7dafd478dab110
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-07-11 12:45:17 +0530

    how validation works in react using RHF

--- React/RHF/src/App.jsx
+++ React/RHF/src/App.jsx
@@ -5,18 +5,21 @@ import { useState } from 'react'
 import Card from './components/Card'
 
 const App = () => {
-
+  const [formData, setFormData] = useState([])
   const [toggle, setToggle] = useState(false)
+  console.log(formData)
 
   return (
     <div className='min-h-screen bg-gray-700 text-white px-[3vw] py-1.5'>
       <Nav setToggle={setToggle} />
 
       {toggle ?
-        <Form />
+        <Form setFormData={setFormData} setToggle={setToggle}/>
         :
         <div className='mt-5 flex flex-wrap gap-3'>
-          <Card />
+         {formData.map((val)=>{
+          return <Card val={val} />
+         })}
         </div>}
 
 
--- React/RHF/src/components/Form.jsx
+++ React/RHF/src/components/Form.jsx
@@ -1,39 +1,61 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useForm } from "react-hook-form"
 import { useState } from 'react'
 
-const Form = () => {
-    const [formData, setFormData] = useState({})
-    function getData(e){
-         setFormData({...formData, [e.target.name]:e.target.value})
-         console.log(formData)
-    }
+
+const Form = ({setFormData,setToggle}) => {
+   
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+        } = useForm()
+
+    let inpRef = useRef({})
+    console.log('App Rendering...')
+        console.log(errors)
+
     return (
         <div className='flex justify-center items-center h-[40vw]'>
-            <form 
-            // onSubmit={(e)=>{
-            //     e.preventDefault()
-            //   console.log(formData)  
-            // }}
-            className='bg-gray-950 flex flex-col gap-4 w-[400px] border-2 border-blue-400 p-5'>
+            <form
+                ref={inpRef}
+                onSubmit={handleSubmit((data)=>{
+                    setFormData((prev)=> [...prev, data])
+                    setToggle((prev)=> !prev)
+                    reset()
+                })}
+                className='bg-gray-950 flex flex-col gap-4 w-[400px] border-2 border-blue-400 p-5'>
                 <input
-                name='name'
-                onChange={getData}
-                className='border-2 border-blue-400 rounded px-3.5 py-2' type="text" placeholder='Name' />
+                    name='name'
+                    {...register('name',{
+                        required:"Name is Required"
+                    })}
+                    className='border-2 border-blue-400 rounded px-3.5 py-2' type="text" placeholder='Name' />
+                {errors.name && <p className='text-red-500'>{errors.name.message}</p>} 
                 <input
-                name='email'
-                onChange={getData}
-                className='border-2 border-blue-400 rounded px-3.5 py-2' type="mail" placeholder='Email' />
-                <input 
-                name='phone'
-                onChange={getData}
-                className='border-2 border-blue-400 rounded px-3.5 py-2' type="number" placeholder='Phone' />
-                <input 
-                name='url'
-                onChange={getData}
-                className='border-2 border-blue-400 rounded px-3.5 py-2' type="url" placeholder='Image' />
-                <button 
-                className='bg-blue-400 rounded px-3.5 py-1.5 active:scale-98'>Add User</button>
+                    name='email'
+                   {...register('email',{
+                        required:"email is Required"
+                    })}
+                    className='border-2 border-blue-400 rounded px-3.5 py-2' type="mail" placeholder='Email' />
+                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                <input
+                    name='phone'
+                    {...register('phone',{
+                        required:"phone is Required"
+                    })}
+                    className='border-2 border-blue-400 rounded px-3.5 py-2' type="number" placeholder='Phone' />
+                    {errors.phone && <p className='text-red-500'>{errors.phone.message}</p>}
+                <input
+                    name='url'
+                    {...register('url',{
+                        required:"url is Required"
+                    })}
+                    className='border-2 border-blue-400 rounded px-3.5 py-2' type="url" placeholder='Image' />
+                    {errors.url && <p className='text-red-500'>{errors.url.message}</p>}
+                <button
+                    className='bg-blue-400 rounded px-3.5 py-1.5 active:scale-98'>Add User</button>
             </form>
         </div>
     )
