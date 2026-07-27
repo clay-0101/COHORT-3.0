1c40db757fb08c4c00bebcd46e0a8efe7935a522
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-07-27 12:17:36 +0530

    Add gaurd route

--- REDUX/Quiz-App/src/router/AppRouter.jsx
+++ REDUX/Quiz-App/src/router/AppRouter.jsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router'
+import { useSelector } from 'react-redux'
 import MainLayout from '../layout/MainLayout'
 import HomePage from '../pages/home/HomePage'
 import QuizPage from '../pages/quiz/QuizPage'
@@ -6,40 +7,45 @@ import SavedPage from '../pages/saved/SavedPage'
 import QuizForm from '../pages/quiz/QuizForm'
 import ScoreScreen from '../pages/quiz/ScoreScreen'
 
-const AppRouter = () => {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <MainLayout />,
-            children: [
-                {
-                    path: "",
-                    element: <HomePage />,
-                },
-                {
-                    path: "quiz",
-                    element: <QuizPage />,
-                    children : [
-                        {
-                            path : "",
-                            element : <QuizForm/>
-                        }
-                    ]
-                },
-                
-                {
-                    path: "score",
-                    element: <ScoreScreen />,
-                },
-                
-                {
-                    path: "saved",
-                    element: <SavedPage />,
-                }
-            ]
-        }])
 
-    return <RouterProvider router={router} />
+function ProtectedSavedPage() {
+  const isQuizStarted = useSelector((state) => state.quiz.isQuizStarted)
+  return isQuizStarted ? <Navigate to="/quiz" replace /> : <SavedPage />
+}
+
+const AppRouter = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "",
+          element: <HomePage />,
+        },
+        {
+          path: "quiz",
+          element: <QuizPage />,
+          children: [
+            {
+              path: "",
+              element: <QuizForm />
+            }
+          ]
+        },
+        {
+          path: "score",
+          element: <ScoreScreen />,
+        },
+        {
+          path: "saved",
+          element: <ProtectedSavedPage />, // guarded route
+        }
+      ]
+    }
+  ])
+
+  return <RouterProvider router={router} />
 }
 
 export default AppRouter
